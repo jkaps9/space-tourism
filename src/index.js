2a16e2ff0c1615f479b86a7d057897edcc410f13
Josh <15219491+jkaps9@users.noreply.github.com>
2025-07-02 23:53:48 -0400

    add display/hide functionality for mobile menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./styles.css";
+
+const hamburger = document.querySelector("#hamburger");
+const closeMenu = document.querySelector("#close-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+hamburger.addEventListener("click", () => {
+  mobileMenu.classList.remove("hidden");
+});
+
+closeMenu.addEventListener("click", () => {
+  mobileMenu.classList.add("hidden");
+});
